a5a3fd62d659939677ede4d9bfaf41d3607990d5
Shane Becker <veganstraightedge@gmail.com>
2017-02-22 23:51:37 -0800

    Remove initializers to migrate one by one

--- config/initializers/heroku_resque_autoscaler.rb
+++
@@ -1,4 +0,0 @@
-HerokuResqueAutoscaler.configure do |config|
-  config.heroku_api_key  = ENV["HEROKU_API_KEY"]
-  config.heroku_app_name = ENV["HEROKU_APP_NAME"]
-end
--- config/initializers/postres.rb
+++
@@ -1,3 +0,0 @@
-# ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.tap do |klass|
-#   klass::OID.register_type('hstore', klass::OID::Identity.new)
-# end
