732ae8f0ca34709f83b13f03cb1373295c527638
Shane Becker <veganstraightedge@gmail.com>
2017-03-02 01:43:25 -0800

    Use radio buttons instead of select for age range

--- app/models/age_range.rb
+++ app/models/age_range.rb
@@ -2,11 +2,4 @@ class AgeRange < ApplicationRecord
   has_many :users
 
   validates :name, presence: true, uniqueness: true
-
-  class << self
-    def options_for_select
-      all.map{ |d| [d.name.capitalize, d.id] }
-    end
-  end
-
 end
