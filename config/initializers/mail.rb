1330bda0753c009528ace4b74ca75b93a0210ac7
Shane Becker <veganstraightedge@gmail.com>
2017-06-06 15:48:22 -0700

    Change default site email address

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,11 @@
 unless Rails.env.test?
   ActionMailer::Base.smtp_settings = {
-    :port =>           '25',
-    :address =>        'email-smtp.us-west-2.amazonaws.com',
-    :user_name =>      ENV['SMTP_USERNAME'],
-    :password =>       ENV['SMTP_PASSWORD'],
-    :domain =>         'kypsy.com',
-    :authentication => :plain
+    port:           '25',
+    address:        'email-smtp.us-west-2.amazonaws.com',
+    user_name:      ENV['SMTP_USERNAME'],
+    password:       ENV['SMTP_PASSWORD'],
+    domain:         'kypsy.com',
+    authentication: :plain
   }
   ActionMailer::Base.delivery_method = :smtp
 end
