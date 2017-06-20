5774d05d0db80e16bd2399fae7734a6ba97ba2d4
Shane Becker <veganstraightedge@gmail.com>
2017-06-20 11:19:25 -0700

    Update photo UI [close #21]

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,19 +3,17 @@ class WelcomeController < ApplicationController
 
   def index
     if logged_in?
-      @title = "People Using #{t(:brand)}"
-      @users = current_user.viewable_users.order('created_at desc').paginate(page: params[:page] ||= 1)
+      @title            = "People Using #{t(:brand)}"
+      @users            = current_user.viewable_users.order('created_at desc').paginate(page: params[:page] ||= 1)
       @crushers         = current_user.crushers
       @crushes          = current_user.crushes - current_user.matches
       @bookmarked_users = current_user.bookmarked_users
       @matches          = current_user.matches
       @avatar_size      = :square
-      @slug     = "people"
+      @slug             = "people"
       return render "/welcome/dashboard", layout: "application"
     else
       @slug  = "welcome"
-      @users = User.visible.featured.shuffle.first(12)
-
       return render layout: "welcome"
     end
   end
